860818880be2448873c3626914a1ac660bb84857
fhochleitner <felix.hochleitner@outlook.com>
2022-03-16 16:27:07 +0100

    fix working directory

--- git-workflows/bin/git-workflow.sh
+++ git-workflows/bin/git-workflow.sh
@@ -50,11 +50,15 @@ git_clone() {
 }
 
 git_checkout() {
-  git checkout ${BRANCH} --git-dir "${WORKSPACE}/${REPO_NAME}"
+  cd "${WORKSPACE}/${REPO_NAME}" \
+  && git checkout ${BRANCH} \
+  && cd cd || exit 1
 }
 
 extract_git_commit() {
-  COMMIT_HASH=$(git rev-parse --short HEAD --git-dir "${WORKSPACE}/${REPO_NAME}")
+  cd "${WORKSPACE}/${REPO_NAME}" \
+  && COMMIT_HASH=$(git rev-parse --short HEAD --git-dir "${WORKSPACE}/${REPO_NAME}") \
+  cd || exit 1
   echo "${COMMIT_HASH}" > "${WORKSPACE}/commit_hash"
 }
 
