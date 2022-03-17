6611c3eaa490a451e3edadf822dbb3bc95c6c5ed
fhochleitner <felix.hochleitner@outlook.com>
2022-03-17 10:31:56 +0100

    don't allow custom settings location

--- maven/bin/mvn-wrapper.sh
+++ maven/bin/mvn-wrapper.sh
@@ -3,37 +3,8 @@
 SETTINGS_LOCATION=/maven/maven-settings.xml
 SETTINGS_STRING=""
 
-######################   handle options ###################
-
-handle_options() {
-#  OPTS=$(getopt -o hcb:u:w: -l help,checkout,branch:,url:,workspace:)
-local opts=$(getopt -o s: -l settings -- "$@")
-local opts_return=$?
-
-if [[ ${opts_return} != 0 ]]; then
-    echo
-    (>&2 echo "failed to fetch options via getopt")
-    echo
-    return ${opts_return}
-fi
-
-eval set -- "$opts"
-while true ; do
-  case "$1" in
-    --settings | -s)
-      SETTINGS_LOCATION="${2}"
-      shift 2
-      ;;
-    *)
-      break
-      ;;
-  esac
-done
-
-}
 
 main(){
-  handle_options "$@"
 
   if [ -f "${SETTINGS_LOCATION}" ]; then
     SETTINGS_STRING="-s ${SETTINGS_LOCATION}"
