f2e984be40b787b96759fd291c29310f4e0bf2ea
fhochleitner <felix.hochleitner@outlook.com>
2022-03-21 13:53:58 +0100

    updated slack-notification entry point

--- slack-notifications/bin/entrypoint.sh
+++ slack-notifications/bin/entrypoint.sh
@@ -5,16 +5,13 @@ source /etc/slack-config
 export SLACK_ICON="https://github.com/fhochleitner/logos/raw/main/argo.png"
 export SLACK_USERNAME="argo-workflows"
 export SLACK_MARKDOWN=true
+export SLACK_COLOR="#228b22"
 export SLACK_WEBHOOK
-SLACK_COLOR="#228b22"
 
 #if [[ "$*" != "null" ]]; then
-#  SLACK_COLOR="#ff0033"
 #  #TODO: trim leading and trailing '"' | replace '\"' with '"'
 #  FAILURES=$(echo "${FAILURES}" | jq -r '.[] | "Failed Step: \(.displayName) Message: \(.message)"')
 #  SLACK_MESSAGE="${SLACK_MESSAGE}${FAILURES}"
 #fi
-
-export SLACK_COLOR
 
 /bin/slack-notify
