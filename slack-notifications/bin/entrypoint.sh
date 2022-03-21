27668ce073229a7398333c4a08744445c0430134
fhochleitner <felix.hochleitner@outlook.com>
2022-03-21 13:27:46 +0100

    temporarily disable listing of build errors in notification

--- slack-notifications/bin/entrypoint.sh
+++ slack-notifications/bin/entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
 source /etc/slack-config
-SLACK_COLOR="#228b22"
+
 export SLACK_ICON="https://github.com/fhochleitner/logos/raw/main/argo.png"
-export SLACK_WEBHOOK
 export SLACK_USERNAME="argo-workflows"
 export SLACK_MARKDOWN=true
+export SLACK_WEBHOOK
+SLACK_COLOR="#228b22"
 
-if [[ "$*" != "null" ]]; then
-  SLACK_COLOR="#ff0033"
-  FAILURES=$(echo "$*" | jq -r '.[] | "Failed Step: \(.displayName) Message: \(.message)"')
-  SLACK_MESSAGE="${SLACK_MESSAGE}${FAILURES}"
-fi
+#if [[ "$*" != "null" ]]; then
+#  SLACK_COLOR="#ff0033"
+#  #TODO: trim leading and trailing '"' | replace '\"' with '"'
+#  FAILURES=$(echo "${FAILURES}" | jq -r '.[] | "Failed Step: \(.displayName) Message: \(.message)"')
+#  SLACK_MESSAGE="${SLACK_MESSAGE}${FAILURES}"
+#fi
+
 export SLACK_COLOR
 
 /bin/slack-notify
