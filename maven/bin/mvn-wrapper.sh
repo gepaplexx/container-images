8e6006c30c5bb158fa3b6375a2b939f351450dbf
fhochleitner <felix.hochleitner@outlook.com>
2022-05-16 09:57:07 +0200

    GPX-339 add fail-at-end option by default to maven build.
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- maven/bin/mvn-wrapper.sh
+++ maven/bin/mvn-wrapper.sh
@@ -10,7 +10,7 @@ main(){
     SETTINGS_STRING="-s ${SETTINGS_LOCATION}"
   fi
   echo "mvn $* $SETTINGS_STRING"
-  bash -c "mvn $* ${SETTINGS_STRING}"
+  bash -c "mvn $* ${SETTINGS_STRING} -fae"
 }
 
 main "$*"
