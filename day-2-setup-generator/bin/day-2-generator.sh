beef8d8ccf3176960c0373997eb849fa119ebd54
fhochleitner <felix.hochleitner@outlook.com>
2022-05-19 16:54:43 +0200

    GPX-335 Cluster-Setup Aufteilung
    typo bei Variablen gefixed.
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- day-2-setup-generator/bin/day-2-generator.sh
+++ day-2-setup-generator/bin/day-2-generator.sh
@@ -262,21 +262,21 @@ function configureGepaplexxCicdTools() {
     replace '$GEPAPLEXX_CICD_TOOLS_ARGOCD_ROUTE_HOSTNAME:$GEPAPLEXX_CICD_TOOLS_ROLLOUTS_ROUTE_HOSTNAME:$GEPAPLEXX_CICD_TOOLS_WORKFLOWS_ROUTE_HOSTNAME'
 
     printf "generating sealed secret values for gepaplexx-cicd-repository..."
-    export GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_SSHPRIVATEKEY=$(echo GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_SSHPRIVATEKEY | base64 -w 0)
-    export GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_USERNAME=$(echo $GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_USERNAME | base64 -w 0)
+    export GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_SSHPRIVATEKEY=$(echo GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_SSHPRIVATEKEY | base64 -w 0)
+    export GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_USERNAME=$(echo $GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_USERNAME | base64 -w 0)
 
     cat templates/secret-cicd-repository-git.yaml.TEMPLATE \
-        | envsubst '$GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_SSHPRIVATEKEY:$GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_USERNAME' \
+        | envsubst '$GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_SSHPRIVATEKEY:$GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_USERNAME' \
         | kubeseal --cert generated/${ENV}.crt -o yaml > generated/gepaplexxcicd-repository-secret.yaml
 
     [[ $? = 0 ]] && printSuccess || printFailureAndExit "Generating"
     printf "Replacing parameters in values-${ENV}.yaml..."
 
-    export GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_SSHPRIVATEKEY=$(cat generated/gepaplexxcicd-repository-secret.yaml | grep sshPrivateKey | cut -d ':' -f 2 | xargs)
-    export GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_USERNAME=$(cat generated/gepaplexxcicd-repository-secret.yaml | grep username | cut -d ':' -f 2 | xargs)
+    export GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_SSHPRIVATEKEY=$(cat generated/gepaplexxcicd-repository-secret.yaml | grep sshPrivateKey | cut -d ':' -f 2 | xargs)
+    export GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_USERNAME=$(cat generated/gepaplexxcicd-repository-secret.yaml | grep username | cut -d ':' -f 2 | xargs)
     export GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_ENABLED=${GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_ENABLED}
 
-    replace '$GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_SSHPRIVATEKEY:$GITHUB_CICD_TOOLS_WORFKLOWREPOSITORY_USERNAME:$GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_ENABLED'
+    replace '$GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_SSHPRIVATEKEY:$GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_USERNAME:$GITHUB_CICD_TOOLS_WORKFLOWREPOSITORY_ENABLED'
 
     printf "Cleanup..."
     rm generated/gepaplexxcicd-repository-secret.yaml
