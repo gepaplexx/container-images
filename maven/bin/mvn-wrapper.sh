38f954b3fd769d7317b88cc560170b0e69b4d338
fhochleitner <felix.hochleitner@gepardec.com>
2023-07-04 13:43:17 +0200

    disable strict checksum validation in maven build
    Signed-off-by: fhochleitner <felix.hochleitner@gepardec.com>

--- maven/bin/mvn-wrapper.sh
+++ maven/bin/mvn-wrapper.sh
@@ -13,7 +13,8 @@ main(){
   fi
   printf "%s [maven-wrapper] %s" "$(date '+%Y-%m-%d %T')" "mvn $* $SETTINGS_STRING"
 
-  bash -c "mvn $* -D org.slf4j.simpleLogger.showDateTime=true -D org.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss' ${SETTINGS_STRING} -fae -C"
+#  bash -c "mvn $* -D org.slf4j.simpleLogger.showDateTime=true -D org.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss' ${SETTINGS_STRING} -fae -C"
+  bash -c "mvn $* -D org.slf4j.simpleLogger.showDateTime=true -D org.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss' ${SETTINGS_STRING} -fae"
 }
 
 main "$*"
