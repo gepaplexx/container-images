a6ec7ef80c30fef871b2da6f6b31130e049d20dd
fhochleitner <felix.hochleitner@outlook.com>
2023-10-10 12:42:40 +0200

    Update README.md added formatting to mvn version command
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- maven/bin/mvn-wrapper.sh
+++ maven/bin/mvn-wrapper.sh
@@ -11,8 +11,8 @@ main(){
   else
     SETTINGS_STRING="-s /usr/share/maven/conf/settings.xml"
   fi
+  mvn --version | while IFS= read -r line; do printf '%s [maven-wrapper] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done
   printf "%s [maven-wrapper] %s" "$(date '+%Y-%m-%d %T')" "mvn $* $SETTINGS_STRING"
-  bash -c "mvn --version"
   bash -c "mvn $* -D org.slf4j.simpleLogger.showDateTime=true -D org.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss' ${SETTINGS_STRING} -fae"
 }
 
