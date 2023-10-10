0d40ea0c938b4eb4d113abc52fa9519371f60095
fhochleitner <felix.hochleitner@outlook.com>
2023-10-10 12:16:51 +0200

    add output of maven version before invoking other maven goals for demonstration purposes
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- maven/bin/mvn-wrapper.sh
+++ maven/bin/mvn-wrapper.sh
@@ -12,8 +12,7 @@ main(){
     SETTINGS_STRING="-s /usr/share/maven/conf/settings.xml"
   fi
   printf "%s [maven-wrapper] %s" "$(date '+%Y-%m-%d %T')" "mvn $* $SETTINGS_STRING"
-
-#  bash -c "mvn $* -D org.slf4j.simpleLogger.showDateTime=true -D org.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss' ${SETTINGS_STRING} -fae -C"
+  bash -c "mvn --version"
   bash -c "mvn $* -D org.slf4j.simpleLogger.showDateTime=true -D org.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss' ${SETTINGS_STRING} -fae"
 }
 
