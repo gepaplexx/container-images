c65180d75ec6d3ecba0ab7b63b7f2b26c5bc482e
fhochleitner <felix.hochleitner@outlook.com>
2024-01-30 10:17:57 +0100

    GPX-546 [CICD] Unterstützung für Gradle Builds
    bash not available in container
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- gradle/bin/gradle-wrapper.sh
+++ gradle/bin/gradle-wrapper.sh
@@ -4,7 +4,7 @@ main(){
 
   gradle --version | while IFS= read -r line; do printf '%s [gradle-wrapper] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line"; done
   printf "%s [gradle-wrapper] %s" "$(date '+%Y-%m-%d %T')" "gradle $* "
-  bash -c "gradle $* -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss'"
+  sh -c "gradle $* -Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat='yyyy-MM-dd HH:mm:ss'"
 }
 
 main "$*"
